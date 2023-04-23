e3e406c0099fbde504ca6348bcf796f9339f158a
mikibak <mikibak123@gmail.com>
2023-04-23 21:38:12 +0200

    Implement compareTo()

--- Project1/src/main/java/org/example/Mage.java
+++ Project1/src/main/java/org/example/Mage.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import java.util.Objects;
 import java.util.Set;
 
-public class Mage {
+public class Mage implements Comparable{
     private String name;
     private int level;
     private double power;
     private Set<Mage> apprentices;
+    private String compareOptions = "natural";
 
     @Override
     public boolean equals(Object o) {
@@ -32,5 +34,35 @@ public class Mage {
         }
 
         return result;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o == this)
+            return 0;
+        if (!(o instanceof Mage))
+            return -1;
+        Mage other = (Mage)o;
+        if(Objects.equals(compareOptions, "natural")) {
+            if(this.name.length() > other.name.length()) {
+                return 1;
+            } else if(this.name.length() < other.name.length()) {
+                return -1;
+            } else {
+                if(this.level > other.level) {
+                    return 1;
+                } else if(this.level < other.level) {
+                    return -1;
+                } else {
+                    if(this.power > other.power) {
+                        return 1;
+                    } else if(this.power < other.power) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
+            }
+        }
     }
 }
