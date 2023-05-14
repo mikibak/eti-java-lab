d4bfb5c5498a291ce71e1705283af053e9049367
mikibak <mikibak123@gmail.com>
2023-05-14 17:16:02 +0200

    Socket server without threads

---
+++ Project3/java-client/src/main/java/org/example/Messsage.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class Messsage implements Serializable {
+    private int number;
+    private String content;
+//setters and getters
+}
