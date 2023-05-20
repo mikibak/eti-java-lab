82355d6d0e1bca5fbdc7e685ac59d4b12c85fded
mikibak <mikibak123@gmail.com>
2023-05-20 22:13:44 +0200

    Revert "Start project 4"
    This reverts commit 7330e88bf82139b5a8851110dc0b35c48e9bd618.

--- Project4/src/main/java/org/example/Mage.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Mage {
-    @Id
-    private String name;
-    private int level;
-    @ManyToOne
-    private Tower tower;
-}
--- Project4/src/main/java/org/example/Tower.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Tower {
-    @Id
-    private String name;
-    private int height;
-    @OneToMany
-    private List<Mage> mages;
-}
