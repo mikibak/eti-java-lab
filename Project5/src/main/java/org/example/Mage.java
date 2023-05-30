b67ff34aa1b79c135bcf93b8806a5210ede10eb9
mikibak <mikibak123@gmail.com>
2023-05-30 11:17:24 +0200

    Implement methods in controller

--- Project5/src/main/java/org/example/Mage.java
+++ Project5/src/main/java/org/example/Mage.java
@@ -4,6 +4,10 @@ public class Mage {
     private String name;
     private int level;
 
+    public Mage(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
     public String getName() {
         return name;
     }
--- Project5/src/main/java/org/example/MageController.java
+++ Project5/src/main/java/org/example/MageController.java
@@ -1,17 +1,34 @@
 package org.example;
 
+import java.util.Optional;
+
 public class MageController {
     private MageRepository repository;
     public MageController(MageRepository repository) {
-//...
+
     }
     public String find(String name) {
-//...
+        Optional<Mage> mage = repository.find(name);
+        if(mage.isEmpty()) {
+            return "not found";
+        } else {
+            return mage.toString();
+        }
     }
     public String delete(String name) {
-//...
+        try {
+            repository.delete(name);
+            return "done";
+        } catch(IllegalArgumentException e) {
+            return "not found";
+        }
     }
     public String save(String name, String level) {
-//...
+        try {
+            repository.save(new Mage(name, Integer.parseInt(level)));
+            return "done";
+        } catch(IllegalArgumentException e) {
+            return "bad request";
+        }
     }
 }
