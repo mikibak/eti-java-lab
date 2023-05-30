8f9e5b7d559c2d5b3fc4af009e4897e4af6e047e
mikibak <mikibak123@gmail.com>
2023-05-30 12:52:28 +0200

    Test delete()

--- Project5/src/test/java/MageControllerTest.java
+++ Project5/src/test/java/MageControllerTest.java
@@ -6,14 +6,13 @@ import org.junit.Test;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class MageControllerTest {
     @Test
     public void find() {
         MageRepository mageRepository = mock(MageRepository.class);
-        Mage mamaszDziubale = new Mage("Mamasz Dziubale", 80));
+        Mage mamaszDziubale = new Mage("Mamasz Dziubale", 80);
         when(mageRepository.find("Mamasz Dziubale")).thenReturn(Optional.of(mamaszDziubale);
         when(mageRepository.find("Magik z Paktofoniki")).thenReturn(Optional.empty());
         MageController mageController = new MageController(mageRepository);
@@ -23,15 +22,20 @@ public class MageControllerTest {
     }
     @Test
     public void delete() {
-        try {
-            repository.delete(name);
-            return "done";
-        } catch(IllegalArgumentException e) {
-            return "not found";
-        }
+        MageRepository mageRepository = mock(MageRepository.class);
+        doThrow(new IllegalArgumentException()).when(mageRepository.delete("Magik z Paktofoniki");
+        MageController mageController = new MageController(mageRepository);
+
+        assertEquals(mageController.delete("Mamasz Dziubale"), "done");
+        assertEquals(mageController.delete("Magik z Paktofoniki"), "not found");
     }
     @Test
     public void save() {
+        MageRepository mageRepository = mock(MageRepository.class);
+        Mage mamaszDziubale = new Mage("Mamasz Dziubale", 80);
+        when(mageRepository.save(mamaszDziubale)).thenReturn().thenThrow(IllegalArgumentException.class);
+        MageController mageController = new MageController(mageRepository);
+
         try {
             repository.save(new Mage(name, Integer.parseInt(level)));
             return "done";
